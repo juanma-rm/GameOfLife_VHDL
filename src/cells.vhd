33112d856740eecd20ffff5a346398ea51c7692b
juanma-rm <juan.reina@citrobits.com>
2023-03-15 18:10:30 +0100

    cells behaviour rebuilt: always taking prev state from input

--- src/cells.vhd
+++ src/cells.vhd
@@ -135,8 +135,9 @@ entity cells is
         clk_i         : in  std_logic;
         rst_i         : in  std_logic;
         next_iter_i   : in  std_logic;
+        cells_arr_i   : in cell_array_t;
         done_o        : out std_logic;
-        cells_arr_o   : out std_logic_vector(num_rows_c*num_cols_c - 1 downto 0)
+        cells_arr_o   : out cell_array_t
     );
 end entity;
 
@@ -146,15 +147,13 @@ end entity;
 
 architecture behavioural of cells is
 
-    signal cells_prev_s : cell_array_t := cells_array_init('1');
-    signal cells_next_s : cell_array_t := (others => (others => (state => '0')));
+    signal cells_prev_s : cell_array_t;
+    signal cells_next_s : cell_array_t;
 
     type state_t is (st_init, st_wait, st_calculate, st_update_prev);
     signal state_s      : state_t;
 
 begin
-    
-    cells_arr_o <= cells_array_to_slv(cells_prev_s);
 
     -- FSM
     process (clk_i)
@@ -165,9 +164,9 @@ begin
             else
                 case state_s is
                     when st_init        => state_s <= st_wait;
-                    when st_wait        => if (next_iter_i = '1') then state_s <= st_calculate; end if;
-                    when st_calculate   => state_s <= st_update_prev;
-                    when st_update_prev => state_s <= st_wait;
+                    when st_wait        => if (next_iter_i = '1') then state_s <= st_update_prev; end if;
+                    when st_update_prev => state_s <= st_calculate;
+                    when st_calculate   => state_s <= st_wait;
                     when others         => state_s <= st_init;
                 end case;
             end if;
@@ -175,6 +174,16 @@ begin
     end process;
 
     done_o <= '1' when (state_s = st_wait) else '0';
+
+    -- Update cells_prev with input data
+    process (clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if    state_s = st_init        then cells_array_set (cells_prev_s, '1');
+            elsif state_s = st_update_prev then cells_prev_s <= cells_arr_i;
+            end if;
+        end if;
+    end process;
 
     -- Update cells_next with next generation states
     process (clk_i)
@@ -189,21 +198,6 @@ begin
             end if;
         end if;
     end process;
-
-    -- Update cells_prev once next generation is calculated
-    process (clk_i)
-    begin
-        if rising_edge(clk_i) then
-            if state_s = st_init then
-                cells_array_set (cells_prev_s, '1');
-            elsif state_s = st_update_prev then
-                for row in 0 to num_rows_c-1 loop
-                    for col in 0 to num_cols_c-1 loop
-                        cells_prev_s(row, col).state <= cells_next_s(row,col).state;
-                    end loop;
-                end loop;
-            end if;
-        end if;
-    end process;
+    cells_arr_o <= cells_next_s;
 
 end architecture;
